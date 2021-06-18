a2c0b59632ba69adfaa65a393f04db2b135bb066
sync0n <43514738+sync0n@users.noreply.github.com>
2021-06-18 10:28:08 +0300

    Update lambdaBuildPipeline.groovy

--- vars/lambdaBuildPipeline.groovy
+++ vars/lambdaBuildPipeline.groovy
@@ -14,7 +14,7 @@ def call(String propFilePath) {
     String zipName = null
 
     pipeline {
-        agent { image 'Dockerfile' }
+        agent { dockerfile true }
         options {
             timeout(time: globalVars.buildPipelineTimeout, unit: 'MINUTES')
         }
